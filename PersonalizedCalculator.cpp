7d2fa6e02b32d1a151736ae538284008c7508564
Priyanshu Pandey <183459105+ceopriyanshu@users.noreply.github.com>
2024-11-30 16:21:56 +0530

    Basic MainMenu Design

--- PersonalizedCalculator.cpp
+++ PersonalizedCalculator.cpp
@@ -1,10 +1,15 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
-	cout<<"Simple Claculator";
-	cout<<"Calculator is not difficult as i think";
+	cout <<"Welcome To Simple Command Line Calculator !" << endl <<" Select an option" << endl;
+	cout <<"1. Basic Claculations (Multiplication, Division, Addition, Subtraction) " << endl;
+	cout <<"2. Tempreture Conversion C <--> F" << endl;
+	cout <<"3. Currency conversion USD <--> INR " << endl;
+	cout <<"4. Length Conversion Meter <--> Kilometers" << endl;
+	cout <<"5. Time Conversion Hours <--> Minutes" << endl;
+	cout <<"6. Recent History" << endl;
+	cout <<"7. Exit Calculator" << endl;
 	
-	return 0;
 }
